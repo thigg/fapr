30d3d5ca5994f1908ff2823bc186d18753ae51e2
thilo <thilo@ati.ttu.ee>
2015-02-13 12:08:18 +0200

    removed u-turn bug:    all_turns included uturns  switched to pools

--- graph/builder.py
+++ graph/builder.py
@@ -3,25 +3,31 @@
 # Copyright (c) 2007-2008 Pedro Matiello <pmatiello@gmail.com>
 # License: MIT (see COPYING file)
 
-# Import graphviz
+# Arguments: arg1 num_cpus to use
+
 import sys
 import itertools
-
-sys.path.append('..')
+from multiprocessing import Pool
+import datetime
 
 # Import pygraph
 from pygraph.classes.digraph import digraph
 from pygraph.algorithms import cycles as cycl
-# from joblib import Parallel, delayed
+
 import multiprocessing
-#from multiprocessing import Pool
+
 
 # Graph creation
 
 network_size = 3
-
+dirs = {-3: "d", -2: "s", -1: "w", 0: "l", 1: "e", 2: "n", 3: "u"}
 
 def test_reachability(tm):
+    """
+    Tests a turnmodel if all nodes are reachable from every node
+    The idea is, that either a turn or it's opposite have to exists
+    Example: NE or EN
+    """
     for a in all_turns:
         b, c = a
         if (b, c) in tm or (c, b) in tm:
@@ -45,26 +51,39 @@ def powerset(seq):
 
 
 def node(dir, x, y, z, pdir):
+    """
+    builds a string for a port
+    :param dir: cardinal direction, one of neswudl
+    :param x: x
+    :param y: y
+    :param z: z
+    :param pdir: in or out
+    :return: a string representing this
+    """
     return dir + str(x) + "." + str(y) + "." + str(z) + pdir
 
-# print node("a",1,2,3,"in")
-# Add nodes and edges
-dirs = {-3: "d", -2: "s", -1: "w", 0: "l", 1: "e", 2: "n", 3: "u"}
-
-# turnmodel.extend([(1, -2), (-2, -1), (-1, 2), (2, 1)])
-# print turnmodel
 
 
 def buildNetwork(network_size, turnmodel):
+    """
+    applies the turnmodel to the whole network size
+    :param network_size:
+    :param turnmodel:
+    :return: a 3d array containing the tm in every field
+    """
     return [[[turnmodel for _ in range(0, network_size)] for _ in range(0, network_size)] for _ in
             range(0, network_size)]
 
 
 lock = multiprocessing.Lock()
 
-
+# checks if the specified turnmodel is cyclefree
+#
 def checkTurnmodelForCycles(network_size, network, tm):
-    "checks the given network represented by a 3d array of turnmodel for cycles, returns the nr of cycles"
+    """
+    checks the given network represented by a 3d array of turnmodel for cycles, returns the nr of cycles
+    todo: remove the tm reference and print in testtm
+    """
     if not test_reachability(tm):
         return None
     gr = digraph()
@@ -87,35 +106,34 @@ def checkTurnmodelForCycles(network_size, network, tm):
                     coord = [x, y, z]
                     # calculate opposite node for my current dir
                     coord[-d - 1] -= 1  # decrement the coord in this direction if d= -1 we have to go -1 in west dir
-                    if coord[-d - 1] < 0: continue  # break this loop if the router does not exist
+                    if coord[-d - 1] < 0:
+                        continue  # break this loop if the router does not exist
+                    if (coord == [x, y, z]):
+                        print '#wtf'
                     gr.add_edge((node(dirs[d], x, y, z, "out"), node(dirs[-d], coord[0], coord[1], coord[2], "in")))
                     gr.add_edge((node(dirs[-d], coord[0], coord[1], coord[2], "out"), node(dirs[d], x, y, z, "in")))
 
+
                 # apply turnmodel to this node
                 for turn in network[x][y][z]:
+                    if (turn[0] == turn[1]):  # uturns shouldnt be allowed
+                        print '#wtf'
                     gr.add_edge((node(dirs[turn[0]], x, y, z, "in"), node(dirs[turn[1]], x, y, z, "out")))
 
     res = len(cycl.find_cycle(gr))
-    gr = None
-    network = None
-    if (res != 0):
+    if res != 0:
         return None
     return (res, tm)
 
 
-turnmodel = []
-# basic turnmodel: straight connections
-for i in range(-3, 4):
-    if i != 0:
-        turnmodel.append((i, -i))  # straight
-
-all_turns = itertools.product([-3, -2, -1, 1, 2, 3], [-3, -2, -1, 1, 2, 3])  # all possible turns
-all_turns = list(set(all_turns).difference(set(turnmodel)))
-
-num_cores = multiprocessing.cpu_count()
 
 
 def testtm(tm):
+    """
+    Tests the given turnmodel
+    :param tm: the turnmodel
+    :return: a tuple of cycle length and the turnmodel, so if first element is 0 it is cycle free and interesting
+    """
     # now build a network from it and add the straight turns before to the tm
     if (len(tm) > 12):  # require at least 12 turns
         turns = set(tm).union(set(turnmodel))
@@ -124,19 +142,42 @@ def testtm(tm):
         return None
 
 
-# for tm in powerset(all_turns):
-#    testtm(tm)
-# the powerset are all possible turns
-
-
-
-from multiprocessing import Pool
-
 if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        num_cores = int(sys.argv[1])
+    else:
+        num_cores = multiprocessing.cpu_count()
+
+    print 'started @ ' + str(datetime.datetime.now()) + " used cpus: " + str(num_cores)
+
+    # Build all_turns
+    turnmodel = []
+    # basic turnmodel: straight connections
+    for i in range(-3, 4):
+        if i != 0:
+            turnmodel.append((i, -i))  # straight
+
+    oports = [-3, -2, -1, 1, 2, 3]  # all non local dirs
+
+    all_turns = itertools.product(oports, oports)  # all possible turns
+    all_turns = list(
+        set(all_turns).difference(set(turnmodel)).difference(zip(oports, oports)))  #remove straights and u-turns
+
+
+
+
+
+
+
     pool = Pool(processes=num_cores)  # use all available CPUs
+
+    #run now in parallel with a chunksize of 1000
     for result in pool.imap_unordered(testtm, powerset(all_turns), chunksize=1000):
         if result != None:
-            print(result)
+            print(result)  # print the found turnmodels
+
+    print 'finished @ ' + str(datetime.datetime.now())
 
 
 
